9d13bc4123b4b7129b6323b157efb1798d902eb1
Jeremy Scheff <jdscheff@gmail.com>
2018-03-07 14:43:17 -0500

    Infrastructure for tests

--- scripts/build.js
+++ scripts/build.js
@@ -2,30 +2,35 @@ const babel = require("rollup-plugin-babel");
 const replace = require("rollup-plugin-replace");
 const rollup = require("rollup");
 
-const sites = ["bbgm"];
+let sites = ["bbgm", "test"];
+
+// Support calling like `yarn run build test` to only build for the test site
+if (process.argv.length > 2) {
+  sites = process.argv.slice(2);
+}
 
 (async () => {
-    for (const site of sites) {
-        const inputOptions =     {
-            input: "src/index.js",
-            plugins: [
-                replace({
-                    include: "src/index.js",
-                    SITE_TO_REPLACE: site,
-                }),
-                babel({
-                    exclude: "src/vendor/**",
-                }),
-            ],
-        };
-        const outputOptions = {
-            name: "bbgmAds",
-            file: `dist/${site}.js`,
-            format: "umd",
-        };
+  for (const site of sites) {
+    const inputOptions = {
+      input: "src/index.js",
+      plugins: [
+        replace({
+          include: "src/index.js",
+          SITE_TO_REPLACE: site
+        }),
+        babel({
+          exclude: "src/vendor/**"
+        })
+      ]
+    };
+    const outputOptions = {
+      name: "bbgmAds",
+      file: `dist/${site}.js`,
+      format: "umd"
+    };
 
-        const bundle = await rollup.rollup(inputOptions);
-        console.log(`Writing ${outputOptions.file}...`)
-        await bundle.write(outputOptions);
-    }
+    const bundle = await rollup.rollup(inputOptions);
+    console.log(`Writing ${outputOptions.file}...`);
+    await bundle.write(outputOptions);
+  }
 })();
---
+++ src/adUnits/test.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    code: "test1",
+    path: "/1/test1",
+    sizes: [[728, 90]],
+    bids: [
+      {
+        bidder: "indexExchange",
+        params: {
+          siteID: "248523",
+          id: "01"
+        }
+      }
+    ]
+  },
+  {
+    code: "test2",
+    path: "/1/test2",
+    sizes: [[300, 250]],
+    bids: [
+      {
+        bidder: "indexExchange",
+        params: {
+          siteID: "248524",
+          id: "02"
+        }
+      }
+    ]
+  }
+];
