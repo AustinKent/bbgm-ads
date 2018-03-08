d28227a1743244427854c7280e601d89d3d36ad5
Jeremy Scheff <jdscheff@gmail.com>
2018-03-07 23:31:57 -0500

    Whoops

--- test/BBGMAds.init.js
+++ test/BBGMAds.init.js
@@ -71,37 +71,3 @@ describe("BBGMAds.init", () => {
     });
   });
 });
-
-describe("BBGMAds.refresh", () => {
-  it("runs", async () => {
-    window.googletag = new GPT();
-    window.googletag._loaded();
-    const bbgmAds = new BBGMAds([], []);
-
-    await bbgmAds.init([]);
-    const res = await bbgmAds.refresh();
-    proclaim(res);
-  });
-
-  it("does nothing if init not called", async () => {
-    window.googletag = new GPT();
-    window.googletag._loaded();
-    const bbgmAds = new BBGMAds([], []);
-
-    const res = await bbgmAds.refresh();
-    proclaim(!res);
-  });
-
-  it("does nothing if init not finished", async () => {
-    window.googletag = new GPT();
-    const bbgmAds = new BBGMAds([], []);
-
-    const promise = bbgmAds.init([]);
-    const res = await bbgmAds.refresh();
-    proclaim(!res);
-
-    window.googletag._loaded();
-
-    await promise;
-  });
-});
