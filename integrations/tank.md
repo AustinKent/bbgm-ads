0b3d917a85b8e40218064b10a0ff2f8023ffa07a
Jeremy Scheff <jdscheff@gmail.com>
2018-07-22 11:47:03 -0400

    Tank

---
+++ integrations/tank.md
@@ -0,0 +1,44 @@
+# Integration guide for Tankathon
+
+## Load async bbgm-ads script
+
+Put this in your `<head>`:
+
+    <script async="async" src="https://www.googletagservices.com/tag/js/gpt.js"></script>
+    <script async="async" src="https://basketball-gm.com/bbgm-ads/tank.js"></script>
+    <script type="text/javascript">
+      var googletag = googletag || {};
+      googletag.cmd = googletag.cmd || [];
+      var bbgmAds = bbgmAds || {};
+      bbgmAds.cmd = bbgmAds.cmd || [];
+    </script>
+
+## Set up banner ad wrappers
+
+Create empty divs to hold the ads.
+
+    <div id="tank-mobile1"></div>
+
+    <div id="tank-mobile3"></div>
+
+    <div id="tank-billboard1"></div>
+
+    <div id="tank-skyscraper1"></div>
+
+    <div id="tank-skyscraper2"></div>
+
+## Display ads
+
+Run this code to display ads in your divs. This must come after all your last ad div. I am assuming that you're already detecting mobile/non-mobile and can put these snippets in appropriate places. If not, let us know.
+
+Use this code for mobile:
+
+    bbgmAds.cmd.push(function () {
+      bbgmAds.init(["tank-mobile1", "tank-mobile3"]);
+    });
+
+And this for non-mobile:
+
+    bbgmAds.cmd.push(function () {
+      bbgmAds.init(["tank-billboard1", "tank-skyscraper1", "tank-skyscraper2"]);
+    });
---
+++ src/sites/tank.js
@@ -0,0 +1,182 @@
+const adUnits = [
+  {
+    code: "tank-mobile1",
+    path: "/42283434/2018-Tank-Mobile1",
+    sizes: [[320, 50]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "259629",
+          size: [320, 50]
+        }
+      },*/
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13659327"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "119999",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "320X50",
+          cp: 558539,
+          ct: 652964
+        }
+      }
+    ]
+  },
+  {
+    code: "tank-mobile3",
+    path: "/42283434/2018-Tank-Mobile3",
+    sizes: [[320, 50]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "259628",
+          size: [320, 50]
+        }
+      },*/
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13659328"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "119999",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "320X50",
+          cp: 558539,
+          ct: 652966
+        }
+      }
+    ]
+  },
+  {
+    code: "tank-billboard1",
+    path: "/42283434/2018-Tank-Billboard1",
+    sizes: [[728, 90]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "259623",
+          size: [728, 90]
+        }
+      },*/
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13659306"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "119999",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "728X90",
+          cp: 558539,
+          ct: 652961
+        }
+      }
+    ]
+  },
+
+  {
+    code: "tank-skyscraper1",
+    path: "/42283434/2018-Tank-Skyscraper1",
+    sizes: [[160, 600]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "259624",
+          size: [160, 600]
+        }
+      },*/
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13659308"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "119999",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "160X600",
+          cp: 558539,
+          ct: 652962
+        }
+      }
+    ]
+  },
+  {
+    code: "tank-skyscraper2",
+    path: "/42283434/2018-Tank-Skyscraper2",
+    sizes: [[160, 600]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "259625",
+          size: [160, 600]
+        }
+      },*/
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13081453"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "119999",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "160X600",
+          cp: 558539,
+          ct: 652963
+        }
+      }
+    ]
+  }
+];
+
+const priceGranularity = "high";
+
+export { adUnits, priceGranularity };
