6d0ca8d0c378419f31dc842f8b9f9f2ffc8715b0
Jeremy Scheff <jdscheff@gmail.com>
2019-01-15 17:13:33 -0500

    Handle the difficult case of 300x250 being a valid size for mobile or desktop, depending on the ad unit

--- src/sites/slam.js
+++ src/sites/slam.js
@@ -80,6 +80,7 @@ const adUnits = [
     code: "div-gpt-ad-1516424492164-2",
     path: "/21680050242/slam_hp_box",
     sizes: [[300, 250]],
+    labelAny: ["desktop", "mobile"],
     bids: [
       {
         bidder: "appnexus",
@@ -113,60 +114,72 @@ const adUnits = [
       }
     ]
   },
+
+  // Because the previous ad unit is desktop and mobile at 300x250 but this one is mobile only, we need to split up this ad unit into two, otherwise it has no way to know that the 300x250 is mobile only. Then set minViewportWidth and maxViewportWidth to be adjacent, so only one of these is ever used
   {
     code: "div-gpt-ad-1516424492164-5",
     path: "/21680050242/slam_hp_tower",
-    sizes: [[160, 600], [300, 250]],
+    sizes: [[160, 600]],
+    minViewportWidth: 641,
+    labelAny: ["desktop"],
     bids: [
       {
         bidder: "appnexus",
-        labelAny: ["desktop"],
         params: {
           placementId: "14693285"
         }
       },
       {
-        bidder: "appnexus",
-        labelAny: ["mobile"],
-        params: {
-          placementId: "14693283"
-        }
-      },
-      {
         bidder: "sovrn",
-        labelAny: ["desktop"],
         params: { tagid: "597377" }
       },
       {
-        bidder: "sovrn",
-        labelAny: ["mobile"],
-        params: { tagid: "597374" }
-      },
-      {
         bidder: "districtm",
-        labelAny: ["desktop"],
         params: {
           placementId: "14708377"
         }
       },
       {
-        bidder: "districtm",
-        labelAny: ["mobile"],
-        params: {
-          placementId: "14708376"
-        }
-      },
-      {
         bidder: "openx",
-        labelAny: ["desktop"],
         params: {
           unit: "540562166",
           delDomain: "ortolani-d.openx.net"
         }
       },
       {
+        bidder: "aol",
+        params: {
+          placement: "5034560",
+          network: "11455.1"
+        }
+      }
+    ]
+  },
+  {
+    code: "div-gpt-ad-1516424492164-5",
+    path: "/21680050242/slam_hp_tower",
+    sizes: [[300, 250]],
+    maxViewportWidth: 640,
+    labelAny: ["mobile"],
+    bids: [
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: "14693283"
+        }
+      },
+      {
+        bidder: "sovrn",
+        params: { tagid: "597374" }
+      },
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "14708376"
+        }
+      },
+      {
         bidder: "openx",
-        labelAny: ["mobile"],
         params: {
           unit: "540562169",
           delDomain: "ortolani-d.openx.net"
@@ -174,15 +187,6 @@ const adUnits = [
       },
       {
         bidder: "aol",
-        labelAny: ["desktop"],
-        params: {
-          placement: "5034560",
-          network: "11455.1"
-        }
-      },
-      {
-        bidder: "aol",
-        labelAny: ["mobile"],
         params: {
           placement: "5034563",
           network: "11455.1"
