147ecece6ec4f9b9481697c8a40cbdcb092b310c
Jeremy Scheff <jdscheff@gmail.com>
2019-01-17 16:30:45 -0500

    Interior pages

--- integrations/slam.md
+++ integrations/slam.md
@@ -17,6 +17,10 @@ Put this in your `<head>`:
 
 Create empty divs to hold the ads.
 
+These are already on your website currently, so really just delete your existing code that you have interacting with them and let our wrapper handle it. Any existing stuff involving `googletag` and `pbjs` - delete it!
+
+### Home page
+
 728x90 on desktop, 320x50 on mobile:
 
     <div id="div-gpt-ad-1516424492164-4"></div>
@@ -34,14 +38,45 @@ Pixels:
     <div id="div-gpt-ad-1516424492164-0" style="height:1px; width:1px;position: absolute;"></div>
     <div id="div-gpt-ad-1516424492164-1" style="height:2px; width:2px;position: absolute;"></div>
 
-These are already on your website currently, so really just delete your existing code that you have interacting with them and let our wrapper handle it. Any existing stuff involving `googletag` and `pbjs` - delete it!
+### Interior page
 
-[Here is an example of how the HTML will look.](slam.html) You can upload this file to your server to confirm that ads are displaying correctly for mobile and desktop.
+728x90 on desktop, 320x50 on mobile:
+
+    <div id="div-gpt-ad-1516424492164-10"></div>
+
+300x250 on desktop and mobile:
+
+    <div id="div-gpt-ad-1516424492164-8"></div>
+
+160x600 on desktop, 300x250 on mobile:
+
+    <div id="div-gpt-ad-1516424492164-11"></div>
+
+Pixels:
+
+    <div id="div-gpt-ad-1516424492164-6" style="height:1px; width:1px;position: absolute;"></div>
+    <div id="div-gpt-ad-1516424492164-7" style="height:2px; width:2px;position: absolute;"></div>
 
 ## Display ads
 
 Run this JavaScript code to display ads in your divs. This must come after all your last ad div is rendered, for instance at the bottom of the HTML page.
 
+### Home page
+
     bbgmAds.cmd.push(function () {
       bbgmAds.init(["div-gpt-ad-1516424492164-0", "div-gpt-ad-1516424492164-1", "div-gpt-ad-1516424492164-2", "div-gpt-ad-1516424492164-4", "div-gpt-ad-1516424492164-5"]);
     });
+
+### Interior page
+
+    bbgmAds.cmd.push(function () {
+      bbgmAds.init(["div-gpt-ad-1516424492164-6", "div-gpt-ad-1516424492164-7", "div-gpt-ad-1516424492164-8", "div-gpt-ad-1516424492164-10", "div-gpt-ad-1516424492164-11"]);
+    });
+
+## Example HTML files
+
+[Home page](slam.html)
+
+[Interior page](slam2.html)
+
+You can upload these files to your server to confirm that ads are displaying correctly for mobile and desktop.
--- src/sites/slam.js
+++ src/sites/slam.js
@@ -1,271 +1,306 @@
-const adUnits = [
-  // In theory this ad unit should be able to be configured just once, but for some reason I don't consistently get the size mapping to work, ugh.
-  {
-    code: "div-gpt-ad-1516424492164-4",
-    path: "/21680050242/slam_hp_lb",
-    sizes: [[728, 90]],
-    minViewportWidth: 641,
-    labelAny: ["desktop"],
-    bids: [
-      {
-        bidder: "ix",
-        params: {
-          siteId: "334667",
-          size: [728, 90]
-        }
-      },
-      {
-        bidder: "appnexus",
-        params: {
-          placementId: "14693280"
-        }
-      },
-      {
-        bidder: "sovrn",
-        params: { tagid: "597371" }
-      },
-      {
-        bidder: "districtm",
-        params: {
-          placementId: "14708373"
-        }
-      },
-      {
-        bidder: "conversant",
-        params: {
-          site_id: "122929",
-          secure: 1
-        }
-      },
-      {
-        bidder: "openx",
-        params: {
-          unit: "540562167",
-          delDomain: "ortolani-d.openx.net"
-        }
-      },
-      {
-        bidder: "aol",
-        params: {
-          placement: "5034561",
-          network: "11455.1"
-        }
-      }
-    ]
-  },
-  {
-    code: "div-gpt-ad-1516424492164-4",
-    path: "/21680050242/slam_hp_lb",
-    sizes: [[320, 50]],
-    maxViewportWidth: 640,
-    labelAny: ["mobile"],
-    bids: [
-      {
-        bidder: "ix",
-        params: {
-          siteId: "334670",
-          size: [320, 50]
-        }
-      },
-      {
-        bidder: "appnexus",
-        params: {
-          placementId: "14693287"
-        }
-      },
-      {
-        bidder: "sovrn",
-        params: { tagid: "597376" }
-      },
-      {
-        bidder: "districtm",
-        params: {
-          placementId: "14708378"
-        }
-      },
-      {
-        bidder: "conversant",
-        params: {
-          site_id: "122929",
-          secure: 1
-        }
-      },
-      {
-        bidder: "openx",
-        params: {
-          unit: "540562171",
-          delDomain: "ortolani-d.openx.net"
-        }
-      },
-      {
-        bidder: "aol",
-        params: {
-          placement: "5034564",
-          network: "11455.1"
-        }
-      }
-    ]
-  },
-  {
-    code: "div-gpt-ad-1516424492164-2",
-    path: "/21680050242/slam_hp_box",
-    sizes: [[300, 250]],
-    labelAny: ["desktop", "mobile"],
-    bids: [
-      {
-        bidder: "ix",
-        params: {
-          siteId: "334668",
-          size: [300, 250]
-        }
-      },
-      {
-        bidder: "appnexus",
-        params: {
-          placementId: "14693282"
-        }
-      },
-      {
-        bidder: "sovrn",
-        params: { tagid: "597373" }
-      },
-      {
-        bidder: "districtm",
-        params: {
-          placementId: "14708375"
-        }
-      },
-      {
-        bidder: "conversant",
-        params: {
-          site_id: "122929",
-          secure: 1
-        }
-      },
-      {
-        bidder: "openx",
-        params: {
-          unit: "540562165",
-          delDomain: "ortolani-d.openx.net"
-        }
-      },
-      {
-        bidder: "aol",
-        params: {
-          placement: "5034559",
-          network: "11455.1"
-        }
-      }
-    ]
-  },
+const getAdUnits = type => {
+  let codes;
+  let paths;
+  if (type === "home") {
+    codes = {
+      lp: "div-gpt-ad-1516424492164-4",
+      box: "div-gpt-ad-1516424492164-2",
+      tower: "div-gpt-ad-1516424492164-5"
+    };
+    paths = {
+      lp: "/21680050242/slam_hp_lb",
+      box: "/21680050242/slam_hp_box",
+      tower: "/21680050242/slam_hp_tower"
+    };
+  } else if (type === "interior") {
+    codes = {
+      lp: "div-gpt-ad-1516424492164-10",
+      box: "div-gpt-ad-1516424492164-8",
+      tower: "div-gpt-ad-1516424492164-11"
+    };
+    paths = {
+      lp: "/21680050242/slam_interior_lb",
+      box: "/21680050242/slam_interior_box",
+      tower: "/21680050242/slam_interior_tower"
+    };
+  } else {
+    throw new Error(`Invalid type: "${type}"`);
+  }
 
-  // Because the previous ad unit is desktop and mobile at 300x250 but this one is mobile only, we need to split up this ad unit into two, otherwise it has no way to know that the 300x250 is mobile only. Then set minViewportWidth and maxViewportWidth to be adjacent, so only one of these is ever used
-  {
-    code: "div-gpt-ad-1516424492164-5",
-    path: "/21680050242/slam_hp_tower",
-    sizes: [[160, 600]],
-    minViewportWidth: 641,
-    labelAny: ["desktop"],
-    bids: [
-      {
-        bidder: "ix",
-        params: {
-          siteId: "334669",
-          size: [160, 600]
+  return [
+    // In theory this ad unit should be able to be configured just once, but for some reason I don't consistently get the size mapping to work, ugh.
+    {
+      code: codes.lp,
+      path: paths.lp,
+      sizes: [[728, 90]],
+      minViewportWidth: 641,
+      labelAny: ["desktop"],
+      bids: [
+        {
+          bidder: "ix",
+          params: {
+            siteId: "334667",
+            size: [728, 90]
+          }
+        },
+        {
+          bidder: "appnexus",
+          params: {
+            placementId: "14693280"
+          }
+        },
+        {
+          bidder: "sovrn",
+          params: { tagid: "597371" }
+        },
+        {
+          bidder: "districtm",
+          params: {
+            placementId: "14708373"
+          }
+        },
+        {
+          bidder: "conversant",
+          params: {
+            site_id: "122929",
+            secure: 1
+          }
+        },
+        {
+          bidder: "openx",
+          params: {
+            unit: "540562167",
+            delDomain: "ortolani-d.openx.net"
+          }
+        },
+        {
+          bidder: "aol",
+          params: {
+            placement: "5034561",
+            network: "11455.1"
+          }
         }
-      },
-      {
-        bidder: "appnexus",
-        params: {
-          placementId: "14693285"
+      ]
+    },
+    {
+      code: codes.lp,
+      path: paths.lp,
+      sizes: [[320, 50]],
+      maxViewportWidth: 640,
+      labelAny: ["mobile"],
+      bids: [
+        {
+          bidder: "ix",
+          params: {
+            siteId: "334670",
+            size: [320, 50]
+          }
+        },
+        {
+          bidder: "appnexus",
+          params: {
+            placementId: "14693287"
+          }
+        },
+        {
+          bidder: "sovrn",
+          params: { tagid: "597376" }
+        },
+        {
+          bidder: "districtm",
+          params: {
+            placementId: "14708378"
+          }
+        },
+        {
+          bidder: "conversant",
+          params: {
+            site_id: "122929",
+            secure: 1
+          }
+        },
+        {
+          bidder: "openx",
+          params: {
+            unit: "540562171",
+            delDomain: "ortolani-d.openx.net"
+          }
+        },
+        {
+          bidder: "aol",
+          params: {
+            placement: "5034564",
+            network: "11455.1"
+          }
         }
-      },
-      {
-        bidder: "sovrn",
-        params: { tagid: "597377" }
-      },
-      {
-        bidder: "districtm",
-        params: {
-          placementId: "14708377"
+      ]
+    },
+    {
+      code: codes.box,
+      path: paths.box,
+      sizes: [[300, 250]],
+      labelAny: ["desktop", "mobile"],
+      bids: [
+        {
+          bidder: "ix",
+          params: {
+            siteId: "334668",
+            size: [300, 250]
+          }
+        },
+        {
+          bidder: "appnexus",
+          params: {
+            placementId: "14693282"
+          }
+        },
+        {
+          bidder: "sovrn",
+          params: { tagid: "597373" }
+        },
+        {
+          bidder: "districtm",
+          params: {
+            placementId: "14708375"
+          }
+        },
+        {
+          bidder: "conversant",
+          params: {
+            site_id: "122929",
+            secure: 1
+          }
+        },
+        {
+          bidder: "openx",
+          params: {
+            unit: "540562165",
+            delDomain: "ortolani-d.openx.net"
+          }
+        },
+        {
+          bidder: "aol",
+          params: {
+            placement: "5034559",
+            network: "11455.1"
+          }
         }
-      },
-      {
-        bidder: "conversant",
-        params: {
-          site_id: "122929",
-          secure: 1
+      ]
+    },
+
+    // Because the previous ad unit is desktop and mobile at 300x250 but this one is mobile only, we need to split up this ad unit into two, otherwise it has no way to know that the 300x250 is mobile only. Then set minViewportWidth and maxViewportWidth to be adjacent, so only one of these is ever used
+    {
+      code: codes.tower,
+      path: paths.tower,
+      sizes: [[160, 600]],
+      minViewportWidth: 641,
+      labelAny: ["desktop"],
+      bids: [
+        {
+          bidder: "ix",
+          params: {
+            siteId: "334669",
+            size: [160, 600]
+          }
+        },
+        {
+          bidder: "appnexus",
+          params: {
+            placementId: "14693285"
+          }
+        },
+        {
+          bidder: "sovrn",
+          params: { tagid: "597377" }
+        },
+        {
+          bidder: "districtm",
+          params: {
+            placementId: "14708377"
+          }
+        },
+        {
+          bidder: "conversant",
+          params: {
+            site_id: "122929",
+            secure: 1
+          }
+        },
+        {
+          bidder: "openx",
+          params: {
+            unit: "540562166",
+            delDomain: "ortolani-d.openx.net"
+          }
+        },
+        {
+          bidder: "aol",
+          params: {
+            placement: "5034560",
+            network: "11455.1"
+          }
         }
-      },
-      {
-        bidder: "openx",
-        params: {
-          unit: "540562166",
-          delDomain: "ortolani-d.openx.net"
+      ]
+    },
+    {
+      code: codes.tower,
+      path: paths.tower,
+      sizes: [[300, 250]],
+      maxViewportWidth: 640,
+      labelAny: ["mobile"],
+      bids: [
+        {
+          bidder: "ix",
+          params: {
+            siteId: "334671",
+            size: [160, 600]
+          }
+        },
+        {
+          bidder: "appnexus",
+          params: {
+            placementId: "14693283"
+          }
+        },
+        {
+          bidder: "sovrn",
+          params: { tagid: "597374" }
+        },
+        {
+          bidder: "districtm",
+          params: {
+            placementId: "14708376"
+          }
+        },
+        {
+          bidder: "conversant",
+          params: {
+            site_id: "122929",
+            secure: 1
+          }
+        },
+        {
+          bidder: "openx",
+          params: {
+            unit: "540562169",
+            delDomain: "ortolani-d.openx.net"
+          }
+        },
+        {
+          bidder: "aol",
+          params: {
+            placement: "5034563",
+            network: "11455.1"
+          }
         }
-      },
-      {
-        bidder: "aol",
-        params: {
-          placement: "5034560",
-          network: "11455.1"
-        }
-      }
-    ]
-  },
-  {
-    code: "div-gpt-ad-1516424492164-5",
-    path: "/21680050242/slam_hp_tower",
-    sizes: [[300, 250]],
-    maxViewportWidth: 640,
-    labelAny: ["mobile"],
-    bids: [
-      {
-        bidder: "ix",
-        params: {
-          siteId: "334671",
-          size: [160, 600]
-        }
-      },
-      {
-        bidder: "appnexus",
-        params: {
-          placementId: "14693283"
-        }
-      },
-      {
-        bidder: "sovrn",
-        params: { tagid: "597374" }
-      },
-      {
-        bidder: "districtm",
-        params: {
-          placementId: "14708376"
-        }
-      },
-      {
-        bidder: "conversant",
-        params: {
-          site_id: "122929",
-          secure: 1
-        }
-      },
-      {
-        bidder: "openx",
-        params: {
-          unit: "540562169",
-          delDomain: "ortolani-d.openx.net"
-        }
-      },
-      {
-        bidder: "aol",
-        params: {
-          placement: "5034563",
-          network: "11455.1"
-        }
-      }
-    ]
-  },
+      ]
+    }
+  ];
+};
+
+const adUnits = [
+  ...getAdUnits("home"),
+  ...getAdUnits("interior"),
   {
     code: "div-gpt-ad-1516424492164-0",
     path: "/21680050242/slam_hp_1x1",
@@ -275,6 +310,18 @@ const adUnits = [
   {
     code: "div-gpt-ad-1516424492164-1",
     path: "/21680050242/slam_hp_2x2",
+    sizes: [[2, 2]],
+    labelAny: ["mobile", "desktop"]
+  },
+  {
+    code: "div-gpt-ad-1516424492164-6",
+    path: "/21680050242/slam_interior_1x1",
+    sizes: [[1, 1]],
+    labelAny: ["mobile", "desktop"]
+  },
+  {
+    code: "div-gpt-ad-1516424492164-7",
+    path: "/21680050242/slam_interior_2x2",
     sizes: [[2, 2]],
     labelAny: ["mobile", "desktop"]
   }
