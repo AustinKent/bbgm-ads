c0f1f9d4dfffc4815f731c36cf6bbef38d54d6fa
Jeremy Scheff <jdscheff@gmail.com>
2019-05-29 16:10:14 -0400

    FBGM placeholder

---
+++ src/sites/fbgm.js
@@ -0,0 +1,381 @@
+const adUnits = [
+  {
+    code: "bbgm-ads-top",
+    path: "/42283434/2019-FBGM-Billboard1",
+    sizes: [[728, 90]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "248523",
+          size: [728, 90]
+        }
+      },
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: "12941329"
+        }
+      },
+      {
+        bidder: "sovrn",
+        params: { tagid: "547545" }
+      },
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "12861126"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "116969",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "728X90",
+          cp: 558539,
+          ct: 657114
+        }
+      },
+      {
+        bidder: "openx",
+        params: {
+          unit: "540271653",
+          delDomain: "ortolani-d.openx.net"
+        }
+      },
+      {
+        bidder: "aol",
+        params: {
+          placement: "4933983",
+          network: "11455.1"
+        }
+      },
+      {
+        bidder: "33across",
+        params: {
+          siteId: "cgglT8l6Or6ykzaKkv7mNO",
+          productId: "siab"
+        }
+      },
+      {
+        bidder: "sonobi",
+        params: {
+          placement_id: "aed64fa8d07ee2c34eb5"
+        }
+      }*/
+    ]
+  },
+  {
+    code: "bbgm-ads-bottom1",
+    path: "/42283434/2019-FBGM-Rectangle1",
+    sizes: [[300, 250]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "248524",
+          size: [300, 250]
+        }
+      },
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: "12941330"
+        }
+      },
+      {
+        bidder: "sovrn",
+        params: { tagid: "547546" }
+      },
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "12861124"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "116969",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "300X250",
+          cp: 558539,
+          ct: 657115
+        }
+      },
+      {
+        bidder: "openx",
+        params: {
+          unit: "540271655",
+          delDomain: "ortolani-d.openx.net"
+        }
+      },
+      {
+        bidder: "aol",
+        params: {
+          placement: "4933982",
+          network: "11455.1"
+        }
+      },
+      {
+        bidder: "33across",
+        params: {
+          siteId: "cF5QU4l6Or6ykiaKlId8sQ",
+          productId: "siab"
+        }
+      },
+      {
+        bidder: "sonobi",
+        params: {
+          placement_id: "23f25428b4393782fca3"
+        }
+      }*/
+    ]
+  },
+  {
+    code: "bbgm-ads-bottom2",
+    path: "/42283434/2019-FBGM-Rectangle2",
+    sizes: [[300, 250]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "248525",
+          size: [300, 250]
+        }
+      },
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: "12941331"
+        }
+      },
+      {
+        bidder: "sovrn",
+        params: { tagid: "547547" }
+      },
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "12861125"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "116969",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "300X250",
+          cp: 558539,
+          ct: 657116
+        }
+      },
+      {
+        bidder: "openx",
+        params: {
+          unit: "540271656",
+          delDomain: "ortolani-d.openx.net"
+        }
+      },
+      {
+        bidder: "aol",
+        params: {
+          placement: "4933984",
+          network: "11455.1"
+        }
+      },
+      {
+        bidder: "33across",
+        params: {
+          siteId: "cOCD3Ol6Or6AuJaKkGJozW",
+          productId: "siab"
+        }
+      },
+      {
+        bidder: "sonobi",
+        params: {
+          placement_id: "3957a4b79a0fa704871d"
+        }
+      }*/
+    ]
+  },
+  {
+    code: "bbgm-ads-skyscraper",
+    path: "/42283434/2019-FBGM-Skyscraper1",
+    sizes: [[160, 600]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "272588",
+          size: [160, 600]
+        }
+      },
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: "13350681"
+        }
+      },
+      {
+        bidder: "sovrn",
+        params: { tagid: "566843" }
+      },
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13350683"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "116969",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "160X600",
+          cp: 558539,
+          ct: 657117
+        }
+      },
+      {
+        bidder: "openx",
+        params: {
+          unit: "540271658",
+          delDomain: "ortolani-d.openx.net"
+        }
+      },
+      {
+        bidder: "aol",
+        params: {
+          placement: "4933985",
+          network: "11455.1"
+        }
+      },
+      {
+        bidder: "33across",
+        params: {
+          siteId: "cU7ueWl6Or6ykzaKkv7mNO",
+          productId: "siab"
+        }
+      },
+      {
+        bidder: "sonobi",
+        params: {
+          placement_id: "d7bb963d56d34244350e"
+        }
+      }*/
+    ]
+  },
+  {
+    code: "bbgm-ads-mobile",
+    path: "/42283434/2019-FBGM-Mobile1",
+    sizes: [[320, 50]],
+    bids: [
+      /*{
+        bidder: "ix",
+        params: {
+          siteId: "272588",
+          size: [320, 50]
+        }
+      },
+      {
+        bidder: "appnexus",
+        params: {
+          placementId: "15292983"
+        }
+      },
+      {
+        bidder: "sovrn",
+        params: { tagid: "603124" }
+      },
+      {
+        bidder: "districtm",
+        params: {
+          placementId: "13350683"
+        }
+      },
+      {
+        bidder: "conversant",
+        params: {
+          site_id: "116969",
+          secure: 1
+        }
+      },
+      {
+        bidder: "pulsepoint",
+        params: {
+          cf: "160X600",
+          cp: 558539,
+          ct: 685985
+        }
+      },
+      {
+        bidder: "openx",
+        params: {
+          unit: "540271658",
+          delDomain: "ortolani-d.openx.net"
+        }
+      },
+      {
+        bidder: "aol",
+        params: {
+          placement: "4933985",
+          network: "11455.1"
+        }
+      },
+      {
+        bidder: "33across",
+        params: {
+          siteId: "cU7ueWl6Or6ykzaKkv7mNO",
+          productId: "siab"
+        }
+      },
+      {
+        bidder: "sonobi",
+        params: {
+          placement_id: "d7bb963d56d34244350e"
+        }
+      }*/
+    ]
+  }
+];
+
+const dfpCurrency = "CAD";
+
+const priceGranularity = "high";
+
+const publisherName = "Football GM";
+
+const pubwiseSite = undefined;
+
+const sizeConfig = undefined;
+
+export {
+  adUnits,
+  dfpCurrency,
+  priceGranularity,
+  publisherName,
+  pubwiseSite,
+  sizeConfig
+};
